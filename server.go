8db303bac391fdad92451416df12daa4277900b2
Anar Bayramov <anar@Anars-MacBook-Pro.local>
2023-09-30 17:23:11 +0400

    server.go completed

--- server.go
+++ server.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 	"strconv"
 )
 
+// function handles the client connection
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
+	//buffer to read data
 	buffer := make([]byte, 1024)
 	n, err := conn.Read(buffer)
 	if err != nil {
@@ -16,16 +19,41 @@ func handleConnection(conn net.Conn) {
 		return
 	}
 
-	input, err := strconv.Atoi(string(buffer[:n]))
+	input, err := strconv.Atoi(string(buffer[:n])) //converting data to int
 	if err != nil {
 		fmt.Println("Can't convert to int:", err)
 		return
 	}
 
 	response := input * 2
-	conn.Write([]byte(strconv.Itoa(response)))
+	conn.Write([]byte(strconv.Itoa(response))) //send back response
 }
 
 func main() {
-	// don't forget to run a func here!!!
+	//Checking command line arguments
+	if len(os.Args) != 2 {
+		fmt.Println("Using: server <port>")
+		os.Exit(1)
+	}
+
+	port := os.Args[1]
+	//listen for connections from that port
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		fmt.Println("Can't start the server:", err)
+		os.Exit(1)
+	}
+
+	defer listener.Close()
+	fmt.Println("Server is listening on port", port)
+
+	//Taking in connections to handle in separate goroutines
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Can't accept connection:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
 }
