70a9dd29f0f5cbe53e12244b1324965308fe7927
Anar Bayramov <anar@Anars-MacBook-Pro.local>
2023-09-30 17:44:45 +0400

    setting up client application

---
+++ client.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+)
+
+func main() {
+	//Checking command line arguments
+	if len(os.Args) != 3{
+		fmt.Println("<server:port> <number>")
+		os.Exit(1)
+	}
+
+	//Taking server addr and number from user input
+	servAddr := os.Args[1]
+	number := os.Args[2]
+
+	//Make a connection to server
+	conn, err := net.Dial("tcp", servAddr)
+	if err != nil{
+		fmt.Println("Can't connect to server:", err)
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	//Send the number in the form of the string
+	_, err = conn.Write([]byte(number))
+	if err != nil{
+		fmt.Println("Can't send data:", err)
+		os.Exit(1)
+	}
+
+	//read the response
+	buffer := make([]byte, 1024)
+	n, err != conn.Read(bubuffer)
+	if err != nil{
+		fmt.Println("Can't read response:", err)
+		os.Exit(1)
+	}
+
+	//Convert the response
+	response := string(buffer[:n])
+	fmt.Println("Server response:", response)
+}
+
